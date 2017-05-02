ac040d03d67a01911290fa89bc576f7160722f0c
Roderick Monk <Rod@Rodericks-MacBook-Pro.local>
2017-05-02 12:04:50 -0700

    First post

--- server-src/client-src/ng-components/ContactUs/ContactUsController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-angular.module('ttc')
-	.controller("ContactUsController", ['$scope', '$http', ($scope, $http) =>
-
-		$http.get('ng-components/ContactUs/ttc_exec.json')
-			.then(response => $scope.executive = response.data)]);
